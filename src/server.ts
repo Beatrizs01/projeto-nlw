f919e578e70ca0c03f6a50d414b82f51adf33858
Beatriz Santos <beatriz.s.s0104@gmail.com>
2024-06-09 11:22:44 -0300

    update

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,23 @@
 import { fastify } from "fastify";
+
 import { fastifySwagger } from "@fastify/swagger";
 import { fastifySwaggerUi } from "@fastify/swagger-ui";
-import { jsonSchemaTransform, serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
+import { fastifyCors } from "@fastify/cors";
+
+import { jsonSchemaTransform, serializerCompiler, validatorCompiler, ZodTypeProvider } from 'fastify-type-provider-zod';
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
 import { getAttendeeBadge } from "./routes/get-attendee-badge";
 import { checkIn } from "./routes/check-in";
 import { getEventAttendees } from "./routes/get-event-attendees";
+import { errorHandler } from "./error-handler";
 
+export const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-
-const app = fastify()
+app .register(fastifyCors, {
+    origin: '*',
+})
 
 app.register(fastifySwagger, {
     swagger: {
@@ -41,7 +47,8 @@ app.register(getAttendeeBadge)
 app.register(checkIn)
 app.register(getEventAttendees)
 
+app.setErrorHandler(errorHandler)
 
-app.listen({ port: 3333 }).then(() => { 
+app.listen({ port: 3333, host: '0.0.0.0' }).then(() => { 
     console.log('Http server running!')
 })
